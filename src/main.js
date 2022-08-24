612bfb17ce72ad40fadf56c0f22f042c2be913c8
maicol hernandez <maykolandres4@gmail.com>
2022-08-24 01:43:11 -0500

    added interactivity

--- src/main.js
+++ src/main.js
@@ -7,16 +7,55 @@ const API = axios.create({
     }
 });
 
+//dynamic functions
+
+searchBtn.addEventListener("click", () => {
+    searchBar.classList.remove("inactive");
+});
+
+window.addEventListener("mouseup", (e) => {
+    if (e.target != searchBar) {
+        searchBar.classList.add("inactive");
+    }
+});
+
+//hidden menu
+
+function openMenu() {
+    hiddenMenuOpen.style.marginRight = "-2%";
+}
+
+window.addEventListener("mouseup", (e) => {
+    if (e.target != hiddenMenuOpen) {
+        closeMenu();
+    }
+});
+
+function closeMenu() {
+    hiddenMenuOpen.style.marginRight = "-200%";
+}
+
+//calls to APÍ
+
 async function getMovies() {
     const results = await API("/films");
 
     const films = results.data.slice(0, 6);
     films.forEach(film => {
 
-        const title = document.querySelector(".title");
+        const mainSectionMovies = document.querySelector(".slider-track");
 
-        title.textContent = film.title;
+        const mainSectionMoviesContainer = document.createElement("div")
+        mainSectionMoviesContainer.classList.add("slide");
 
+        const mainSectionMoviesImg = document.createElement("img");
+        mainSectionMoviesImg.classList.add("main-section-img");
+        mainSectionMoviesImg.classList.add("main-section-img-size");
+        mainSectionMoviesImg.setAttribute("src", "https://image.tmdb.org/t/p/bestv2/" + film.image);
 
+        mainSectionMoviesContainer.appendChild(mainSectionMoviesImg);
+        mainSectionMovies.appendChild(mainSectionMoviesContainer);
     });
 }
+
+getMovies()
---
+++ src/nodes.js
@@ -0,0 +1,5 @@
+//nav bar nodes
+
+const searchBar = document.querySelector(".header-search-input");
+const searchBtn = document.querySelector(".header-search-btn");
+const hiddenMenuOpen = document.querySelector(".hidden-menu");
