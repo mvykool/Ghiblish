367e1889d5c855f55c3dbdedb706155077fdc135
mvykool <maykolandres4@gmail.com>
2023-09-10 23:48:49 -0500

    created button

--- ghi/ui-lib/src/lib/button/button.component.ts
+++ ghi/ui-lib/src/lib/button/button.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ghi-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent {}
+export class ButtonComponent implements OnInit {
+  @Input('text') text: string = ''
+
+
+  ngOnInit(): void {}
+}
