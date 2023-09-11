9d7a94e0019a69f48467fa9432ebe37a6484b313
mvykool <maykolandres4@gmail.com>
2023-09-10 21:51:58 -0500

    created button compo

--- workspace/projects/ghibli-ui/src/lib/ghibli-ui.module.ts
+++ workspace/projects/ghibli-ui/src/lib/ghibli-ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { GhibliUiComponent } from './ghibli-ui.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
 @NgModule({
   declarations: [
-    GhibliUiComponent
+    GhibliUiComponent,
+    ButtonComponent
   ],
   imports: [
   ],
