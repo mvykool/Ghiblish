1b3224bfedfe9cee8934b04e1ba7a3328a3346e2
mvykool <maykolandres4@gmail.com>
2023-09-12 01:00:05 -0500

    created more stories

--- ghi/ui-lib/src/lib/index.ts
+++ ghi/ui-lib/src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './ui-lib.module';
 
 export * from './button/button.component';
+export * from './navbar/navbar.component';
--- ghi/ui-lib/src/lib/ui-lib.module.ts
+++ ghi/ui-lib/src/lib/ui-lib.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent]
+  declarations: [ButtonComponent, NavbarComponent],
+  exports: [ButtonComponent, NavbarComponent],
 })
 export class UiLibModule {}
