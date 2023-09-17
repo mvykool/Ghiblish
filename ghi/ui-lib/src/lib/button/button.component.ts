c7083674fa80032590cfea005b562444b9a07059
mvykool <maykolandres4@gmail.com>
2023-09-17 03:40:31 -0500

    added more styles

--- ghi/ui-lib/src/lib/button/button.component.ts
+++ ghi/ui-lib/src/lib/button/button.component.ts
@@ -14,7 +14,15 @@ export class ButtonComponent {
   @Input()
   type: 'primary' | 'secondary' | 'third' = 'primary';
 
-  public get classes(): string[] {
-    return getStyles(this.type);
-  }
+
+  @Input()
+  size: 'default' | 'big' | 'small' = 'default';
+
+
+public get classes(): string[] {
+  const typeClass = this.type ? `type-${this.type}` : ''; // Add a class based on the 'type' property
+  const sizeClass = this.size ? `size-${this.size}` : ''; // Add a class based on the 'size' property
+  return [typeClass, sizeClass];
 }
+
+};
