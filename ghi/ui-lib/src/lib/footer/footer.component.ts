ae2c82cc0aa408a8c52a50be4acdc97b6112dccd
mvykool <maykolandres4@gmail.com>
2023-10-01 14:09:42 -0500

    added new component

---
+++ ghi/ui-lib/src/lib/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ghi-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {}
--- ghi/ui-lib/src/lib/ui-lib.module.ts
+++ ghi/ui-lib/src/lib/ui-lib.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, NavbarComponent],
+  declarations: [ButtonComponent, NavbarComponent, FooterComponent],
   exports: [ButtonComponent, NavbarComponent],
 })
 export class UiLibModule {}
