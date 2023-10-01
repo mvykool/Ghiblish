2bcc7f7796422873a9485f58fa34f8b815027922
mvykool <maykolandres4@gmail.com>
2023-10-01 14:27:11 -0500

    added new component

---
+++ ghi/ui-lib/src/lib/footer/footer.component.stories.ts
@@ -0,0 +1,24 @@
+import { Story, Meta } from '@storybook/angular';
+import { FooterComponent } from './footer.component';
+
+export default {
+  title: 'lib/navbar',
+  component: FooterComponent,
+  args: {},
+} as Meta;
+
+const Template: Story<FooterComponent> = (args: FooterComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: 'secondary',
+};
+
+export const Third = Template.bind({});
+Third.args = {
+  type: 'third',
+};
