b90d770725be5fdc52f4979383a059b76acf5665
mvykool <maykolandres4@gmail.com>
2023-10-01 14:48:22 -0500

    added new component

--- ghi/ui-lib/src/lib/footer/footer.component.ts
+++ ghi/ui-lib/src/lib/footer/footer.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+const getVariants = (...args: string[]) => ['footer', ...args].filter(Boolean);
 
 @Component({
   selector: 'ghi-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  @Input()
+  type: 'default' | 'secondary' | 'third' = 'default';
+
+  public get navbar(): string[] {
+    return getVariants('footer', this.type);
+  }
+}
