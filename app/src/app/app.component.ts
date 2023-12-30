f066a2ee6451153c85095584c3239a5ff50131a6
mvykool <maykolandres4@gmail.com>
2023-12-30 02:36:54 -0500

    got title from api

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,23 +5,26 @@ import { ApiService } from './services/api-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { BannerComponent } from './shared/components/banner/banner.component';
+import { CardsComponent } from './shared/components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, HttpClientModule, NavbarComponent, BannerComponent]
+  imports: [CommonModule, RouterOutlet, HttpClientModule, CardsComponent, NavbarComponent, BannerComponent]
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-  navLinks = ['home', 'contact', 'about us', 'settings'];
-  bgImage = 'https://wallpapercosmos.com/w/full/2/a/b/1198591-3840x2160-desktop-4k-studio-ghibli-background-photo.jpg';
+  title: string = 'app';
+  cards: any[] = []
+  navLinks: string[] = ['home', 'contact', 'about us', 'settings'];
+  bgImage: string = 'https://wallpapercosmos.com/w/full/2/a/b/1198591-3840x2160-desktop-4k-studio-ghibli-background-photo.jpg';
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getData().subscribe(data => {
       console.log(data);
+      this.cards = data;
     })
   }
 }
---
+++ app/src/app/shared/components/cards/cards.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css'
+})
+export class CardsComponent {
+  @Input() title: string = "";
+}
